8f80e9051ea8541723edd742cbfdf1070867c3df
alfarama <albifajarramadha@gmail.com>
2019-11-29 09:08:20 +0700

    lanjutan

--- application/views/admin/tampilan_pemilih.php
+++ application/views/admin/tampilan_pemilih.php
@@ -1,27 +1,32 @@
-<div class="container">
-<a href="<?=base_url('admin/')?>tambah_kode" class="btn btn-success my-3">Tambah</a>
-<table class="table" id="data">
-<div class="my-3 text-center">
-<button type="button" class="btn btn-success mr-3 btn-sm">
-  Terpakai <span class="badge badge-light"><?=$data['terpakai']?></span>
-</button>
+<div class="side-bar float-left">sd</div>
+<div class="main float-right">
+	<div style="margin: 0px 10%">
+		<a href="<?=base_url('admin/')?>tambah_kode" class="btn btn-success my-3">Tambah</a>
+		<table class="table" id="data">
+		<div class="my-3 text-center">
+		<button type="button" class="btn btn-success mr-3 btn-sm">
+		  Terpakai <span class="badge badge-light"><?=$data['terpakai']?></span>
+		</button>
 
-<button type="button" class="btn btn-danger btn-sm">
-  Belum Terpakai <span class="badge badge-light"><?=$data['belum_terpakai']?></span>
-</button>
+		<button type="button" class="btn btn-danger btn-sm">
+		  Belum Terpakai <span class="badge badge-light"><?=$data['belum_terpakai']?></span>
+		</button>
+		</div>
+			<tr>
+				<th>Kode</th>
+				<th>Status</th>
+			</tr>
+			<?php foreach ($data as $row): ?>
+				<tr>
+					<td><?=$row['kode_unik']?></td>
+					<td><?=$row['status_kode']?></td>
+				</tr>
+			<?php endforeach ?>
+		</table>
+	</div>
 </div>
-	<tr>
-		<th>Kode</th>
-		<th>Status</th>
-	</tr>
-	<?php foreach ($data as $row): ?>
-		<tr>
-			<td><?=$row['kode_unik']?></td>
-			<td><?=$row['status_kode']?></td>
-		</tr>
-	<?php endforeach ?>
-</table>
-</div>
+<div></div>
+<div class="p-5 bg-primary" style="clear: both"></div>
 <script type="text/javascript" src="<?= base_url('assets/')?>bootstrap/js/jquery.dataTables.min.js"></script>
 <script type="text/javascript">
   $(document).ready( function () {
--- application/views/core/header_admin.php
+++ application/views/core/header_admin.php
@@ -6,10 +6,22 @@
   <link rel="stylesheet" href="<?= base_url('assets/')?>bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="<?=base_url('assets/')?>font-awesome/css/font-awesome.min.css">  
   <link rel="stylesheet" href="<?= base_url('assets/')?>datatables/css/jquery.dataTables.min.css">
+  <style type="text/css">
+  	.main{
+  		width: 70%;
+  	}
+  	.side-bar{
+  		height: calc(120vh - 2rem);
+  		width: 19%;
+  		background-color: #121267;
+  		position: sticky;
+  		top: 0;
+  	}
+  </style>
 </head>
 <body>
-<a href="<?= base_url('admin/pemilih')?>">pemilih</a>
+<!-- <a href="<?= base_url('admin/pemilih')?>">pemilih</a>
 <br>
 <a href="<?= base_url('admin/tambah_calon')?>">calon</a>
 <br>
-<a href="<?= base_url('admin/statistik')?>">statistik</a>
+<a href="<?= base_url('admin/statistik')?>">statistik</a> -->
